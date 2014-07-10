54e04c52d17d70834b9e8cf837196185fee0c7b8
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2014-07-10 12:18:18 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'ccee95f18c5afe944e1d4d6d4002cd78409b4232f7bac0977f61c855a365075ad540d86901d2f3cda6444fb6b53ac3e1b4fac5b86bbe8a81d582bb05b30ebbb5'
